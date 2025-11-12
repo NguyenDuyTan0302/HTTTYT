8bb0d9a74007dda53446fa2ffe0f8c06caab7550
NguyenDuyTan_AIK2 <93969546+NguyenDuyTan0302@users.noreply.github.com>
2025-11-12 11:19:07 +0700

    Tân updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,20 @@
     return (
       <header className="sticky top-0 z-10 w-full flex items-center justify-between bg-white px-8 py-3 shadow-md">
         <div className="nav-left">
-          <h1 className="text-xl font-semibold text-slate-800">HealthCare Dashboard</h1>
-        </div>
-
-        <div className="nav-right flex items-center gap-4">
           <Link
             to="/home"
             className="text-slate-800 font-medium px-3 py-1.5 rounded-md hover:bg-slate-100 transition"
           >
-            Trang chủ
+            HealthCare Dashboard
           </Link>
+        </div>
+
+        <div className="nav-right flex items-center gap-4">
           <Link
             to="/profile/administrative"
             className="text-slate-800 font-medium px-3 py-1.5 rounded-md hover:bg-slate-100 transition"
           >
-            Profile
+            Workspace
           </Link>
           <img
             src="https://randomuser.me/api/portraits/men/75.jpg"
